daa1c496b98ad68b3a90fe60c688fd9fa9c03d79
uulzhan2007 <uulzhantabyldyeva12@gmail.com>
2025-08-06 23:49:31 +0600

    первый коммит

--- js/home_works.js
+++ js/home_works.js
@@ -1,20 +1,49 @@
-// MOVE BLOCK HW 1.2
+const gmailInput = document.getElementById("gmail_input").value.trim();
+const resultSpan = document.getElementById("gmail_result");
 
-const parentBlock = document.querySelector('.parent_block');
+document.getElementById("gmail_button").addEventListener("click", function () {
+
+    const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+
+    if (gmailRegex.test(gmailInput)) {
+        resultSpan.textContent = "good Gmail!";
+        resultSpan.style.color = "lime";
+    } else {
+        resultSpan.textContent = "bad bro. Используйте @gmail.com";
+        resultSpan.style.color = "red";
+    }
+});
+
+
 const childBlock = document.querySelector('.child_block');
+const parentBlock = document.querySelector('.parent_block');
 
-let positionX = 0
-let positionY = 0
+let positionX = 0;
+let positionY = 0;
 
-const offWidth = parentBlock.offsetWidth - childBlock.offsetWidth
-const offHeight = parentBlock.offsetHeight - childBlock.offsetHeight
+const offsetWidth = parentBlock.clientWidth - childBlock.clientWidth;
+const offsetHeight = parentBlock.clientHeight - childBlock.clientHeight;
 
 const moveBlock = () => {
-    if (positionX < offWidth) positionX++
-    if (positionX >= offWidth && positionY < offHeight) positionY++
-    childBlock.style.left = `${positionX}px`
-    childBlock.style.top = `${positionY}px`
-    requestAnimationFrame(moveBlock)
-}
+    if (positionX < offsetWidth && positionY === 0) {
+        positionX++;
+    } else if (positionX === offsetWidth && positionY < offsetHeight) {
+        positionY++;
+    } else if (positionY === offsetHeight && positionX > 0) {
+        positionX--;
+    } else if (positionX === 0 && positionY > 0) {
+        positionY--;
+    } else {
+        return;
+    }
+
+    childBlock.style.left = `${positionX}px`;
+    childBlock.style.top = `${positionY}px`;
+
+    requestAnimationFrame(moveBlock);
+};
 
 moveBlock();
+
+
+
